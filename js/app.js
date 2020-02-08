ce1f04531fcb59619cca698b759920f0332f32df
Sasa Orasanin <web@sasaorasanin.com>
2020-02-08 19:46:37 +0100

    Changing from jqeury to vue and adding vuex

--- js/app.js
+++ js/app.js
@@ -4,44 +4,43 @@ if (window.location.href.includes('instagram.com')) {
     el.id = 'IGTools';
     document.body.appendChild(el);
     setTimeout(function () {
-        const script = JSON.parse($('body').html().match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1));
+        const script = JSON.parse(document.body.innerHTML.match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1));
 
         Vue.component('ig-tools', {
             template: `
             <div>
             <div id="ig-tools" v-if="toolsForm">
                 <div class="sidebar">
-                    <div class="section active" data-section="user-stats">User statistics</div>
-                    <div class="section" data-section="auto-follow">Auto follow</div>
-                    <div class="section" data-section="auto-unfollow">Auto unfollow</div>
-                    <div class="section" data-section="get-stories">Get stories</div>
-                    <div class="section" data-section="get-posts">Get posts</div>
-                    <div class="section" data-section="direct-messages">Direct messages</div>
+                    <div class="section" :class="section === 'user-stats' ? 'active':''" @click="changeSection('user-stats')">User statistics</div>
+                    <div class="section" :class="section === 'auto-follow' ? 'active':''" @click="changeSection('auto-follow')">Auto follow</div>
+                    <div class="section" :class="section === 'auto-unfollow' ? 'active':''" @click="changeSection('auto-unfollow')">Auto unfollow</div>
+                    <div class="section" :class="section === 'get-stories' ? 'active':''" @click="changeSection('get-stories')">Get stories</div>
+                    <div class="section" :class="section === 'get-posts' ? 'active':''" @click="changeSection('get-posts')">Get posts</div>
+                    <div class="section" :class="section === 'direct-messages' ? 'active':''" @click="changeSection('direct-messages')">Direct messages</div>
                     <hr>
                     <button class="toggleTools" @click="toggleTools">Hide Tools</button>
                 </div>
                 <div class="content">
-                    <div data-section="user-stats" class="active">
-                        <div></div>
-                        <button id="updateStats">Update stats</button>
+                    <div v-if="section === 'user-stats'" :class="section === 'user-stats' ? 'active':''">
+                        <user-stats></user-stats>
                     </div>
-                    <div data-section="get-stories">
+                    <div v-if="section === 'get-stories'" :class="section === 'get-stories' ? 'active':''">
                         <p>Coming soon!</p>
                     </div>
-                    <div data-section="get-posts">
+                    <div v-if="section === 'get-posts'" :class="section === 'get-posts' ? 'active':''">
                         <p>Coming soon!</p>
                     </div>
-                    <div data-section="direct-messages">
+                    <div v-if="section === 'direct-messages'" :class="section === 'direct-messages' ? 'active':''">
                         <p>Coming soon!</p>
                     </div>
-                    <div data-section="auto-follow">
+                    <div v-if="section === 'auto-follow'" :class="section === 'auto-follow' ? 'active':''">
                         <label>
                             <span>Enter seconds until follow:</span>
                             <input class="aigb-setting" :value="$root.$data.igTools.autoFollow.timer / 1000" placeholder="Enter seconds until follow:" data-setting="secondsUntilAction" />
                         </label>
                         Show black list and Follow list and form to add follow list
                     </div>
-                    <div data-section="auto-unfollow">
+                    <div v-if="section === 'auto-unfollow'" :class="section === 'auto-unfollow' ? 'active':''">
                         <label>
                             <span>Enter seconds until unfollow (under 72 could ban you):</span>
                             <input class="aigb-setting" :value="$root.$data.igTools.autoUnfollow.timer / 1000" placeholder="Enter seconds until unfollow:" data-setting="secondsUntilAction" />
@@ -56,14 +55,133 @@ if (window.location.href.includes('instagram.com')) {
             data: function() {
                 return {
                     toolsForm: false,
+                    section: 'user-stats'
                 }
             },
             methods: {
                 toggleTools: function () {
                     this.toolsForm = !this.toolsForm;
+                },
+                changeSection: function (section) {
+                    this.section = section;
                 }
             }
         })
+
+        Vue.component('user-stats', {
+            template: `
+            <div>
+            <div class="users-list">
+                <div class="list-header">Followers: {{ $root.$data.igTools.userStats.followers.length }}</div>
+                <div data-list="followers">
+                    <div class="user" v-for="(user, k) in $root.$data.igTools.userStats.followers" :key="k">
+                        <img :src="user.node.profile_pic_url">
+                        <span>{{ user.node.username }}</span>
+                        <button v-if="!$root.$data.igTools.userStats.followingsIDs.includes(user.node.id) && !user.node.requested_by_viewer" class="follow-btn" @click="follow(user.node.id)">Follow</button>
+                        <button v-if="$root.$data.igTools.userStats.followingsIDs.includes(user.node.id)" class="unfollow-btn" @click="unfollow(user.node.id)">Unfollow</button>
+                        <button v-if="user.node.requested_by_viewer" class="unfollow-btn" @click="unfollow(user.node.id)">Requested</button>
+                    </div>
+                </div>
+            </div>
+            <div class="users-list">
+                <div class="list-header">Followings: {{ $root.$data.igTools.userStats.followings.length }}</div>
+                <div data-list="followings">
+                    <div class="user" v-for="(user, k) in $root.$data.igTools.userStats.followings" :key="k">
+                        <img :src="user.node.profile_pic_url">
+                        <span>{{ user.node.username }}</span>
+                        <button class="unfollow-btn" @click="unfollow(user.node.id)">Unfollow</button>
+                    </div>
+                </div>
+            </div>
+            <div class="users-list">
+                <div class="list-header">Unfollowers: {{ $root.$data.igTools.userStats.unfollowers.length }}</div>
+                <div data-list="unfollowers">
+                    <div class="user" v-for="(user, k) in $root.$data.igTools.userStats.unfollowers" :key="k">
+                        <img :src="user.node.profile_pic_url">
+                        <span>{{ user.node.username }}</span>
+                        <button v-if="!user.node.requested_by_viewer" class="unfollow-btn" @click="unfollow(user.node.id)">Unfollow</button>
+                        <button v-if="user.node.requested_by_viewer" class="unfollow-btn" @click="unfollow(user.node.id)">Requested</button>
+                    </div>
+                </div>
+            </div>
+            <div class="users-list">
+                <div class="list-header">Unfollowings: {{ $root.$data.igTools.userStats.unfollowings.length }}</div>
+                <div data-list="unfollowings">
+                    <div class="user" v-for="(user, k) in $root.$data.igTools.userStats.unfollowings" :key="k">
+                        <img :src="user.node.profile_pic_url">
+                        <span>{{ user.node.username }}</span>
+                        <button v-if="!user.node.requested_by_viewer" class="follow-btn" @click="follow(user.node.id)">Follow</button>
+                        <button v-if="user.node.requested_by_viewer" class="unfollow-btn" @click="unfollow(user.node.id)">Requested</button>
+                    </div>
+                </div>
+            </div>
+            <button @click="updateStats">Update stats</button>
+            </div>
+            `,
+            methods: {
+                updateStats: function () {
+                    this.$root.$data.igTools.userStats.followers = [];
+                    this.$root.$data.igTools.userStats.followersIDs = [];
+                    this.$root.$data.igTools.userStats.followings = [];
+                    this.$root.$data.igTools.userStats.followingsIDs = [];
+                    this.$root.$data.igTools.userStats.unfollowers = [];
+                    this.$root.$data.igTools.userStats.unfollowersIDs = [];
+                    this.$root.$data.igTools.userStats.unfollowings = [];
+                    this.$root.$data.igTools.userStats.unfollowingsIDs = [];
+                    this.fetchFollows(this.$root.$data.igTools.userStats.user);
+                },
+                fetchFollows: function (user, list = 'followers', after = null) {
+                    let vm = this;
+                    axios.get(`https://www.instagram.com/graphql/query/?query_hash=${this.$root.$data.igTools.hashes[list].h}&variables=${encodeURIComponent(JSON.stringify({
+                        "id": user.id,
+                        "include_reel": true,
+                        "fetch_mutual": true,
+                        "first": 51,
+                        "after": after
+                    }))}`).then(function (response) {
+                        console.log(response);
+                        vm.$root.$data.igTools.userStats[list].push(...response.data.data.user[vm.$root.$data.igTools.hashes[list].path].edges);
+                        if (response.data.data.user[vm.$root.$data.igTools.hashes[list].path].page_info.has_next_page) {
+                            setTimeout(function () {
+                                vm.fetchFollows(user, list, response.data.data.user[vm.$root.$data.igTools.hashes[list].path].page_info.end_cursor);
+                            }, 1000);
+                        } else if(list === 'followers') {
+                            setTimeout(function () {
+                                vm.fetchFollows(vm.$root.$data.igTools.userStats.user, 'followings');
+                            }, 1000);
+                        } else {
+                            setTimeout(function () {
+                                vm.$root.$data.igTools.userStats.followersIDs = [];
+                                vm.$root.$data.igTools.userStats.followers.forEach(user => {
+                                    vm.$root.$data.igTools.userStats.followersIDs.push(user.node.id);
+                                });
+                                vm.$root.$data.igTools.userStats.followings.forEach(user => {
+                                    if (!vm.$root.$data.igTools.userStats.followersIDs.includes(user.node.id)) {
+                                        vm.$root.$data.igTools.userStats.unfollowers.push(user);
+                                        vm.$root.$data.igTools.userStats.unfollowersIDs.push(user.node.id);
+                                    }
+                                });
+                                vm.$root.$data.igTools.userStats.followingsIDs = [];
+                                vm.$root.$data.igTools.userStats.followings.forEach(user => {
+                                    vm.$root.$data.igTools.userStats.followingsIDs.push(user.node.id);
+                                });
+                                vm.$root.$data.igTools.userStats.followers.forEach(user => {
+                                    if (!vm.$root.$data.igTools.userStats.followingsIDs.includes(user.node.id)) {
+                                        vm.$root.$data.igTools.userStats.unfollowings.push(user);
+                                        vm.$root.$data.igTools.userStats.unfollowingsIDs.push(user.node.id);
+                                    }
+                                });
+                                // setIGTools();
+                            }, 1000);
+                        }
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                }
+            }
+        })
+
+        // Setting up extension configuration
 
         const app = new Vue({
             el: "#IGTools",
@@ -128,6 +246,12 @@ if (window.location.href.includes('instagram.com')) {
                 },
                 app_init: function () {
                     this.igTools = this.get_tools() ?? this.set_tools();
+                    let vm = this;
+                    axios.get(`https://www.instagram.com/${vm.igTools.script.config.viewer.username}/?__a=1`).then(function (response) {
+                        vm.igTools.userStats.user = response.data.graphql.user;
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
                 }
             },
             mounted: function() {
